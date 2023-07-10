e13c57f1507d5348e12030f25384bdcce7dd503c
Tanupriya Pathak <87934620+tanupriya9102@users.noreply.github.com>
2023-07-10 13:56:16 +0530

    commit

---
+++ Recursion/PERMUTATION_WITH_SPACE.py
@@ -0,0 +1,24 @@
+#when no space required befre 1st letter of string "ABC" => NO SPACE BEFORE A
+
+def pws(ip,op):
+     res=[]
+     if len(ip)==0:
+         res.append(op)
+         return res
+     op1=op2=op
+     op1+=ip[0]
+     op2+="_"
+     op2+=ip[0]
+     ip=ip[1:]
+     res+=pws(ip,op1)
+     res+=pws(ip,op2)
+     return res
+     
+def solve(init_ip):
+    ip=init_ip[1:] 
+    op=init_ip[0]
+    return pws(ip,op)
+    
+print(solve("ABC"))
+        
+    
